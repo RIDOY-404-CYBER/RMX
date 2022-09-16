b6657b9a66d4effea14e465d92be18805235a131
PICCI-RIDOY <107355397+RIDOY-404-CYBER@users.noreply.github.com>
2022-09-17 01:19:46 +0600

    Add files via upload

---
+++ MX-PRO.py
@@ -0,0 +1,18 @@
+import os, platform, time
+print('\n\x1b[1;37m Checking Update...');time.sleep(0.5)
+os.system('git pull')
+try:
+    import gtts
+except IOError:
+    os.system('pip install gtts')
+def Update():
+    exit('\033[1;31m(×) Commands On Update Please Wait For Update ❤ ')
+def RANDOM():
+        bit = platform.architecture()[0]
+        if bit == '64bit':
+            print("\x1b[1;92m Congratulations ! Your Device Support this Tools")
+
+            import Rn64
+        else:
+            exit('\033[1;31m[×] Device Not Support 32bit')
+RANDOM()
