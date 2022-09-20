d326251eb7c93a7ebd31de667cb3aab8aca715ff
PICCI-RIDOY <107355397+RIDOY-404-CYBER@users.noreply.github.com>
2022-09-20 20:32:16 +0600

    Update MX-PRO.py

--- MX-PRO.py
+++ MX-PRO.py
@@ -2,17 +2,17 @@ import os, platform, time
 print('\n\x1b[1;37m Checking Update...');time.sleep(0.5)
 os.system('git pull')
 try:
-    import xox
+    import Rn64
 except IOError:
     os.system('pip install gtts')
 def Update():
     exit('\033[1;31m(×) Commands On Update Please Wait For Update ❤ ')
-def CHECKING():
+def RANDOM():
         bit = platform.architecture()[0]
         if bit == '64bit':
             print("\x1b[1;92m Congratulations ! Your Device Support this Tools")
 
-            import xox
+            import Rn64
         else:
             exit('\033[1;31m[×] Device Not Support 32bit')
-CHECKING()
+RANDOM()
