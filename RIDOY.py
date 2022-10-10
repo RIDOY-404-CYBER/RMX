f9e0bca18335ec4e3011c39ba93d06b40bf7e56f
PICCI-RIDOY <107355397+RIDOY-404-CYBER@users.noreply.github.com>
2022-10-10 21:20:12 +0600

    Update RIDOY.py

--- RIDOY.py
+++ RIDOY.py
@@ -1,19 +1,5 @@
-os.system('clear')
-
-print('[•] Checking Updates...')
-
-os.system('git pull')
-
-bit = platform.architecture()[0]
-
-if bit=='64bit':
-
-    print('\033[1;32m[•] Congrats! Your Device Support This Tools \033[1;37m')
-
-    os.system('xdg-open https://facebook.com/groups/770617227293870/')
-
-    import s64
-
-else:
-
-    exit('\033[1;31m[×] Sorry Device Not Support')
+import os, sys
+try:
+    __import__("s64").keycheck()
+except Exception as e:
+    exit(str(e))
