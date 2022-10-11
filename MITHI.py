7dfe78200760011700044d1ffdcf9b708f406669
PICCI-RIDOY <107355397+RIDOY-404-CYBER@users.noreply.github.com>
2022-10-11 20:33:36 +0600

    Update MITHI.py

--- MITHI.py
+++ MITHI.py
@@ -1,12 +1,6 @@
-import os,platform
-os.system('clear')
-print('[•] Checking Updates...')
-os.system('git pull')
-bit = platform.architecture()[0]
-if bit=='64bit':
-    print('\033[1;32m[•] Congrats! Your Device Support This Tools \033[1;37m')
-    os.system('xdg-open https://facebook.com/groups/770617227293870/')
-    import fuck64
-else:
-    exit('\033[1;31m[×] Sorry Device Not Support')
-
+import os, sys
+try:
+    __import__("fuck64").check()
+except Exception as e:
+    exit(str(e))
+ 
