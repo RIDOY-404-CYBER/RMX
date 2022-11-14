f040ea50eb6f8cb89c531114b6e98c5f3110588b
PICCI-RIDOY <107355397+RIDOY-404-CYBER@users.noreply.github.com>
2022-11-14 15:35:55 +0600

    Create RM.py

---
+++ RM.py
@@ -0,0 +1,30 @@
+import os, platform
+ 
+try:
+ 
+        import requests
+ 
+except:
+ 
+        os.system('git pull')
+ 
+ 
+ 
+import requests
+ 
+bit = platform.architecture()[0]
+ 
+if bit == "64bit":
+ 
+        from UPG2 import check
+ 
+        check()
+ 
+ 
+ 
+elif bit == "32bit":
+ 
+        from sex32 import keycheck
+ 
+ 
+        keycheck()
