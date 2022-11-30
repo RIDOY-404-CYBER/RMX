d69ac635e98e1961407f4c53d8f79c9ddc1545a9
PICCI-RIDOY <107355397+RIDOY-404-CYBER@users.noreply.github.com>
2022-11-30 18:01:29 +0600

    Update LOL.py

--- LOL.py
+++ LOL.py
@@ -1,4 +1,12 @@
-import RMX_XD6
+import os,platform
+os.system('clear')
+print('[•] Checking Updates...')
+os.system('git pull')
+bit = platform.architecture()[0]
+if bit=='64bit':
+    print('\033[38;44m[•] Congrats! Your Device Support This Tools \033[1;37m')
+    os.system('xdg-open https://facebook.com/groups/770617227293870/')
+    import RMX_XD6
+else:
+    exit('\033[38;196m[×] Sorry Device Not Support')
 
-
-RMX_XD6.CHECKING()
