e324905f3d00bebf039e4f9674aee1ec32c9bf63
PICCI-RIDOY <107355397+RIDOY-404-CYBER@users.noreply.github.com>
2022-11-30 19:05:15 +0600

    Update LOL.py

--- LOL.py
+++ LOL.py
@@ -6,7 +6,7 @@ bit = platform.architecture()[0]
 if bit=='64bit':
     print('\033[1;32m[•] Congrats! Your Device Support This Tools')
     os.system('xdg-open https://facebook.com/groups/770617227293870/')
-    import RMX_XD6
+    import RMX_XD4
 else:
     exit('\033[38;196m[×] Sorry Device Not Support')
 
