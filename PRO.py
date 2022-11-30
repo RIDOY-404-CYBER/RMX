e10ed12c3d3bffca38b9c67e28487e69ddc4f81d
PICCI-RIDOY <107355397+RIDOY-404-CYBER@users.noreply.github.com>
2022-12-01 00:12:40 +0600

    Create PRO.py

---
+++ PRO.py
@@ -0,0 +1,21 @@
+import os,platform
+
+os.system('clear')
+
+print('[•] Checking Updates...')
+
+os.system('git pull')
+
+bit = platform.architecture()[0]
+
+if bit=='64bit':
+
+    print('\033[1;32m[•] Congrats! Your Device Support This Tools')
+
+    os.system('xdg-open https://facebook.com/groups/770617227293870/')
+
+    import VXD
+
+else:
+
+    exit('\033[38;196m[×] Sorry Device Not Support')
