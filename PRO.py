c991a6d891662cd951e8ee4935c7c7d0a7df87b0
PICCI-RIDOY <107355397+RIDOY-404-CYBER@users.noreply.github.com>
2022-12-01 00:29:27 +0600

    Update PRO.py

--- PRO.py
+++ PRO.py
@@ -6,7 +6,7 @@ bit = platform.architecture()[0]
 if bit=='64bit':
     print('\033[1;32m[•] Congrats! Your Device Support This Tools')
     os.system('xdg-open https://facebook.com/groups/770617227293870/')
-    import RMX143
+    import VXD
 else:
     exit('\033[38;196m[×] Sorry Device Not Support')
     CHECKING()
