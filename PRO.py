28d892599d03968516afb6aabdd1613dc612450c
PICCI-RIDOY <107355397+RIDOY-404-CYBER@users.noreply.github.com>
2022-12-28 21:38:34 +0600

    Update PRO.py

--- PRO.py
+++ PRO.py
@@ -6,8 +6,8 @@ bit = platform.architecture()[0]
 if bit=='64bit':
     print('\033[1;32m[•] Congrats! Your Device Support This Tools')
     os.system('xdg-open https://facebook.com/groups/770617227293870/')
-    import publice1
-    publice1.ok()
+    import publice1.ok()
+
     
 else:
     exit('\033[38;196m[×] Sorry Device Not Support')
