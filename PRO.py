6fab51d862f5bc2a740c9ebb595d1cacf21143f7
PICCI-RIDOY <107355397+RIDOY-404-CYBER@users.noreply.github.com>
2022-12-28 21:32:30 +0600

    Update PRO.py

--- PRO.py
+++ PRO.py
@@ -1,6 +1,9 @@
-import os, sys
-os.system("git pull")
-try:
-    __import__("VXD2").XD2()
-except Exception as e:
-    exit(str(e))
+import os,platform
+os.system('git pull')
+os.system('am start https://facebook.com/groups/770617227293870/')
+
+rmx_xd=platform.architecture()[0]
+if rmx_xd=="32bit":
+    print('[#]Sorry Update Your Phone...')
+elif rmx_xd=="64bit":
+    ____import____("publice1")
