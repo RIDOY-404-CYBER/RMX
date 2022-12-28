8188ab2f82c7510e9bd54d117a0a39021c3151f8
PICCI-RIDOY <107355397+RIDOY-404-CYBER@users.noreply.github.com>
2022-12-28 21:37:41 +0600

    Update PRO.py

--- PRO.py
+++ PRO.py
@@ -1,10 +1,13 @@
 import os,platform
+os.system('clear')
+print('[•] Checking Updates...')
 os.system('git pull')
-os.system('am start https://facebook.com/groups/770617227293870/')
-
-rmx_xd=platform.architecture()[0]
-if rmx_xd=="32bit":
-    print('[#]Sorry Update Your Phone...')
-elif rmx_xd=="64bit":
-    __import__("publice1")
+bit = platform.architecture()[0]
+if bit=='64bit':
+    print('\033[1;32m[•] Congrats! Your Device Support This Tools')
+    os.system('xdg-open https://facebook.com/groups/770617227293870/')
+    import publice1
     publice1.ok()
+    
+else:
+    exit('\033[38;196m[×] Sorry Device Not Support')
