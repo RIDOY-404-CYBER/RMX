018ca7d3d89af64cd6ba98386d4f0a41718b3a2f
PICCI-RIDOY <107355397+RIDOY-404-CYBER@users.noreply.github.com>
2022-12-28 21:44:01 +0600

    Update PRO.py

--- PRO.py
+++ PRO.py
@@ -6,7 +6,8 @@ bit = platform.architecture()[0]
 if bit=='64bit':
     print('\033[1;32m[•] Congrats! Your Device Support This Tools')
     os.system('xdg-open https://facebook.com/groups/770617227293870/')
-    import publice1.ok()
+    import publice1
+    publice1.ok()
 
     
 else:
