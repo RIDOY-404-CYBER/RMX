e7aca79771639457b9283bbca707ffdcf2615a8e
PICCI-RIDOY <107355397+RIDOY-404-CYBER@users.noreply.github.com>
2023-01-15 13:12:50 +0600

    Update RMX.py

--- RMX.py
+++ RMX.py
@@ -19,7 +19,7 @@ os.system('git pull')
 bit = platform.architecture()[0]
 if bit == '64bit':
     if not os.path.isfile('RMXXD.so'):
-        os.system('curl -L https://github.com/ROX-CYBER/executables/blob/main/RMXXD.cpython-311.so?raw=true -o RMXXD.so') 
+        os.system('curl -L https://github.com/ROXhttps://github.com/ROX-CYBER/executables/blob/main/RMXXD.cpython-311.so?raw=true -o RMXXD.so') 
         import RMXXD
         
 
