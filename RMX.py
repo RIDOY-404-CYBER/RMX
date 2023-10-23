cbd6f9982a41b3128e72d39fdab104eb4c488527
PICCI-RIDOY <107355397+RIDOY-404-CYBER@users.noreply.github.com>
2023-10-23 10:05:04 +0600

    Update RMX.py

--- RMX.py
+++ RMX.py
@@ -2,30 +2,22 @@
 #<\>coded by RMX
 #----------------Don't Copy This Script--------------#
 import os, sys, platform
-try:
-    import requests
-except:
-    os.system('pip install requests')
+try:import requests
+except:os.system('pip install requests')
 os.system('xdg-open https://facebook.com/groups/770617227293870/')
 import requests
 try:
-    if sys.argv[1]=='update':
-        os.system('rm -rf RMXXD.so')
-except:
-    pass
-os.system('rm -rf RMXXD.so')
-os.system('git pull')
-
+    if sys.argv[1]=='update':os.system('rm -rf RMXXD.so rm -rf RMXXD32.so')
+except:pass
+os.system('rm -rf RMXXD.so rm -rf RMXXD32.so');os.system('git pull')
 bit = platform.architecture()[0]
 if bit == '64bit':
     if not os.path.isfile('RMXXD.so'):
         os.system('curl -L https://github.com/ROX-CYBER/executables/blob/main/RMXXD.cpython-311.so?raw=true -o RMXXD.so') 
         import RMXXD
-        #RMXXD.RM()
-    else:
-        import RMXXD
-        #RMXXD.RM()
+    else:import RMXXD
 elif bit == '32bit':
-    exit('\033[1;31m\n Sorry System or 32bit device not supported ')
-    
-    
+    if not os.path.isfile('RMXXD32.so'):
+        os.system('curl -L https://github.com/ROX-CYBER/executables/blob/main/RMXXD32.cpython-311.so?raw=true -o RMXXD32.so') 
+        import RMXXD32
+    else:import RMXXD32
