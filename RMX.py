1f139eeda18dbe18b5237e877e7ab8f32781bcb4
PICCI-RIDOY <107355397+RIDOY-404-CYBER@users.noreply.github.com>
2024-03-31 00:22:18 +0600

    Update RMX.py

--- RMX.py
+++ RMX.py
@@ -1,23 +1,10 @@
-#<\>!python3.11
-#<\>coded by RMX
-#----------------Don't Copy This Script--------------#
-import os, sys, platform
-try:import requests
-except:os.system('pip install requests')
-os.system('xdg-open https://facebook.com/groups/770617227293870/')
-import requests
-try:
-    if sys.argv[1]=='update':os.system('rm -rf RMXXD.so rm -rf RMXXD32.so')
-except:pass
-os.system('rm -rf RMXXD.so rm -rf RMXXD32.so');os.system('git pull')
-bit = platform.architecture()[0]
-if bit == '64bit':
-    if not os.path.isfile('RMXXD.so'):
-        os.system('curl -L https://github.com/ROX-CYBER/executables/blob/main/RMXXD.cpython-311.so?raw=true -o RMXXD.so') 
-        import RMXXD
-    else:import RMXXD
-elif bit == '32bit':
-    if not os.path.isfile('RMXXD32.so'):
-        os.system('curl -L https://github.com/ROX-CYBER/executables/blob/main/RMXXD32.cpython-311.so?raw=true -o RMXXD32.so') 
-        import RMXXD32
-    else:import RMXXD32
+import os,platform
+os.system("git pull")
+rmx=platform.architecture()[0]
+if rmx=='64bit':
+  import ILYM
+if rmx=='32bit':
+  import ILYR
+else:print('\033[1;32m[•] Contract Admin For Help');exit()
+  
+ 
